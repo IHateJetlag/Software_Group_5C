a30abdaf7f8db6ada9a9f24221057665d3ac6af2
tomoya161026 <tomoya161026@icloud.com>
2025-06-15 18:23:40 +0900

    Update GUI.ml

--- GUI/GUI.ml
+++ GUI/GUI.ml
@@ -1,1 +1,16 @@
+コンパイル
+javac prototype/*.java
 
+実行
+java prototype.App
+
+ログイン名　tomoya
+パスワード　tomoya
+
+ログイン名　test1
+パスワード　test1
+
+
+chat <相手名> <メッセージ>: チャットメッセージを送信する。
+appoint <相手名> <月> <日> <タイトル>: 指定した日時に予定を追加する。
+exit: リスナーを終了する。
